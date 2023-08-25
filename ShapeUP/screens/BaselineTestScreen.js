ad683deede0617d0112a8d16204f8c43dd618c2a
Josh Anness <annessjosh@gmail.com>
2023-08-25 11:55:47 -0400

    Baseline Test Screen Changes

--- ShapeUP/screens/BaselineTestScreen.js
+++ ShapeUP/screens/BaselineTestScreen.js
@@ -1,7 +1,6 @@
-// screens/BaselineTestScreen.js
-
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 const BaselineTestScreen = ({ navigation }) => {
   const [age, setAge] = useState("");
@@ -10,41 +9,77 @@ const BaselineTestScreen = ({ navigation }) => {
   const [sex, setSex] = useState("");
 
   const handleBaselineSubmit = () => {
-    // Here, you can send the data to your backend or wherever you want to save it.
-    // For now, we'll just navigate back.
+    // Validate the inputs
+    if (age < 18 || age > 100) {
+      Alert.alert("Invalid Age", "Please enter an age between 18 and 100.");
+      return;
+    }
+
+    if (height < 21 || height > 110) {
+      Alert.alert("Invalid Height", "Please enter a height between 21 and 110 inches.");
+      return;
+    }
+
+    if (!sex || (sex !== "male" && sex !== "female")) {
+      Alert.alert("Invalid Sex", "Please select either Male or Female for sex.");
+      return;
+    }
+
     navigation.goBack();
   };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>Baseline Test</Text>
+
       <TextInput
         style={styles.input}
         placeholder="Age"
         keyboardType="numeric"
         value={age}
-        onChangeText={setAge}
+        onChangeText={(text) => {
+          if (!isNaN(text)) {
+            setAge(text);
+          }
+        }}
       />
+
       <TextInput
         style={styles.input}
-        placeholder="Weight (in kgs)"
+        placeholder="Weight (in lbs)"
         keyboardType="numeric"
         value={weight}
-        onChangeText={setWeight}
+        onChangeText={(text) => {
+          if (!isNaN(text)) {
+            setWeight(text);
+          }
+        }}
       />
+
       <TextInput
         style={styles.input}
-        placeholder="Height (in cms)"
+        placeholder="Height (in inches)"
         keyboardType="numeric"
         value={height}
-        onChangeText={setHeight}
+        onChangeText={(text) => {
+          if (!isNaN(text)) {
+            setHeight(text);
+          }
+        }}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Sex"
-        value={sex}
-        onChangeText={setSex}
-      />
-      <Button title="Submit" onPress={handleBaselineSubmit} />
+
+      <View style={{ width: '95%', height: 50, borderColor: 'gray', borderWidth: 1, borderRadius: 8, marginBottom: 20 }}>
+        <Picker
+          selectedValue={sex}
+          onValueChange={(itemValue) => setSex(itemValue)}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <Picker.Item label="Male" value="male" />
+          <Picker.Item label="Female" value="female" />
+        </Picker>
+      </View>
+
+      <Button title="Submit" onPress={handleBaselineSubmit} style={styles.submitButton} />
     </View>
   );
 };
@@ -53,13 +88,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    padding: 20,
+    padding: '5%',
+    backgroundColor: '#f4f4f8',
+    alignItems: 'center'
+  },
+  header: {
+    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center'
   },
   input: {
+    width: '95%',
     padding: 10,
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 20,
+    borderRadius: 8
+  },
+  submitButton: {
+    marginTop: 10,
   },
 });
 
