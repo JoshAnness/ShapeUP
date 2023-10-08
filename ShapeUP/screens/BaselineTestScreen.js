61b80fa6bca90763fc3014e69addf5ee73791e0f
Josh Anness <annessjosh@gmail.com>
2023-10-08 17:17:31 -0400

    Baseline of BT

--- ShapeUP/screens/BaselineTestScreen.js
+++ ShapeUP/screens/BaselineTestScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button, Alert, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, Alert, ScrollView, TouchableOpacity } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import { auth, db } from '../firebase';
 import { setDoc, doc } from 'firebase/firestore';
@@ -20,6 +20,12 @@ const BaselineTestScreen = ({ navigation }) => {
   const handleNextStep = () => {
     setCurrentStep(currentStep + 1);
   };
+
+  const handlePreviousStep = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1);
+    }
+  };  
 
   const handleBaselineSubmit = async () => {
 
@@ -44,55 +50,61 @@ const BaselineTestScreen = ({ navigation }) => {
     }
   };
 
+  const selectFitnessLevel = (level) => {
+    setExerciseTypes([level]);
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {currentStep === 1 && (
-        <View style={styles.section}>
-          <Text>What type of exercise do you do?</Text>
-          <Checkbox
-            style={styles.checkbox}
-            value={exerciseTypes.includes('Endurance')}
-            onValueChange={() => {
-              if (exerciseTypes.includes('Endurance')) {
-                setExerciseTypes(exerciseTypes.filter(type => type !== 'Endurance'));
-              } else {
-                setExerciseTypes([...exerciseTypes, 'Endurance']);
-              }
-            }}
-          />
-          <Text>Endurance</Text>
-          {/* Add other checkboxes similarly */}
-        </View>
+        <>
+
+        <Text style={styles.mainQuestion}>What is your fitness level?</Text>
+  
+        <TouchableOpacity onPress={() => selectFitnessLevel('Beginner')} style={styles.option}>
+          <Checkbox style={styles.checkbox} value={exerciseTypes.includes('Beginner')} />
+          <View style={styles.textContainer}>
+            <Text style={styles.answerText}>Regularly</Text>
+            <Text style={styles.subtext}>I haven't lifted weights or just started</Text>
+          </View>
+        </TouchableOpacity>
+  
+        <TouchableOpacity onPress={() => selectFitnessLevel('Intermediate')} style={styles.option}>
+          <Checkbox style={styles.checkbox} value={exerciseTypes.includes('Intermediate')} />
+          <View style={styles.textContainer}>
+            <Text style={styles.answerText}>Intermediate</Text>
+            <Text style={styles.subtext}>I've done common weighted exercises</Text>
+          </View>
+        </TouchableOpacity>
+  
+        <TouchableOpacity onPress={() => selectFitnessLevel('Advanced')} style={styles.option}>
+          <Checkbox style={styles.checkbox} value={exerciseTypes.includes('Advanced')} />
+          <View style={styles.textContainer}>
+            <Text style={styles.answerText}>Advanced</Text>
+            <Text style={styles.subtext}>I've been lifting weights for a while</Text>
+          </View>
+        </TouchableOpacity>
+      </>
       )}
+      
       {currentStep === 2 && (
-        <View>
-          <Text>What is your fitness level?</Text>
-          <Picker selectedValue={fitnessLevel} onValueChange={(itemValue) => setFitnessLevel(itemValue)}>
-            <Picker.Item label="Beginner" value="beginner" />
-            <Picker.Item label="Intermediate" value="intermediate" />
-            <Picker.Item label="Advanced" value="advanced" />
-          </Picker>
-        </View>
-      )}
-      {currentStep === 3 && (
         <View style={styles.section}>
-          <Text>What type of equipment do you have?</Text>
-          <Checkbox
-            style={styles.checkbox}
-            value={equipment.includes('Full gym')}
-            onValueChange={() => {
-              if (equipment.includes('Full gym')) {
-                setEquipment(equipment.filter(equip => equip !== 'Full gym'));
-              } else {
-                setEquipment([...equipment, 'Full gym']);
-              }
-            }}
-          />
-          <Text>Full gym</Text>
-          {/* Add other checkboxes similarly */}
-        </View>
+        <Text>What type of equipment do you have?</Text>
+        <Checkbox
+          style={styles.checkbox}
+          value={equipment.includes('Full gym')}
+          onValueChange={() => {
+            if (equipment.includes('Full gym')) {
+              setEquipment(equipment.filter(equip => equip !== 'Full gym'));
+            } else {
+              setEquipment([...equipment, 'Full gym']);
+            }
+          }}
+        />
+        <Text>Full gym</Text>
+      </View>
       )}
-      {/* Continue adding your questions */}
+
       {currentStep < 9 ? (
         <Button title="Next" onPress={handleNextStep} />
       ) : (
@@ -105,25 +117,55 @@ const BaselineTestScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     padding: '5%',
-    backgroundColor: '#f4f4f8',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-  header: {
+  titleBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
     marginBottom: 20,
+    alignItems: 'center',
+  },
+  backArrow: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000000',  // Red color for the back arrow
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  mainQuestion: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#333',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  option: {
+    flexDirection: 'row', 
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15, 
+    borderBottomWidth: 1,
+    borderColor: '#E5E5E5',
+  },
+  checkbox: {
+    marginRight: 10,
+  },
+  textContainer: {
+    flexDirection: 'column',
+    flex: 1, 
   },
   section: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: '100%', // added this to make sure they stretch and align properly
-    marginBottom: 10, // added this for some spacing
-  },
-  checkbox: {
-    margin: 8,
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 10,
   },
   input: {
     width: '95%',
@@ -131,12 +173,12 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 20,
-    borderRadius: 8
+    borderRadius: 8,
   },
   pickerContainer: {
     width: '95%',
     height: 50,
-    marginBottom: 125
+    marginBottom: 125,
   },
   picker: {
     width: '100%',
@@ -145,6 +187,14 @@ const styles = StyleSheet.create({
   submitButton: {
     marginTop: 10,
   },
+  answerText: {
+    fontSize: 19,
+  },
+  subtext: {
+    fontSize: 14, 
+    color: '#888',
+  },
 });
 
 export default BaselineTestScreen;
+
